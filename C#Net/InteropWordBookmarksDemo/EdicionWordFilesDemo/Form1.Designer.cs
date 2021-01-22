13e1a90a4a3adf9e33ac61c577340a6bd26a4b21
Ivan Monreal <ivan.monreal@elizondomx.com>
2021-01-22 08:56:09 -0600

    Adding interop bookmark demo

---
+++ C#Net/InteropWordBookmarksDemo/EdicionWordFilesDemo/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace EdicionWordFilesDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.tbDomicilio = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Location = new System.Drawing.Point(27, 106);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(242, 49);
+            this.btnEditar.TabIndex = 0;
+            this.btnEditar.Text = "Editar documento";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(26, 31);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(242, 20);
+            this.tbName.TabIndex = 1;
+            // 
+            // tbDomicilio
+            // 
+            this.tbDomicilio.Location = new System.Drawing.Point(27, 75);
+            this.tbDomicilio.Name = "tbDomicilio";
+            this.tbDomicilio.Size = new System.Drawing.Size(241, 20);
+            this.tbDomicilio.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Domicilio";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(297, 183);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbDomicilio);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.btnEditar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.TextBox tbDomicilio;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ C#Net/InteropWordBookmarksDemo/EdicionWordFilesDemo/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace EdicionWordFilesDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static string DOCBASE_PATH = Application.StartupPath + @"\PruebaDoc.docx";
+        public static string DOCEDIT_PATH = Application.StartupPath + @"\PruebaDocEdit.pdf";
+        public static string DOMICILIO_TXT = @"[Domicilio]";
+        public static object NAME_BM = @"Nombre";  //BOOKMARK(nombre del bookmark)
+        
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            object ObjMiss = System.Reflection.Missing.Value;   //Objeto para abrir word con parametros default
+
+            Word.Application ObjWord = new Word.Application();  //Abrimos word
+            Word.Document ObjDoc = ObjWord.Documents.Open((object)DOCBASE_PATH, ObjMiss);   //Abrimos documento
+            setTextToBookmark(ObjDoc, NAME_BM, tbName.Text);
+            replaceText(ObjDoc, DOMICILIO_TXT, tbDomicilio.Text);
+            ObjDoc.SaveAs2(DOCEDIT_PATH, Word.WdSaveFormat.wdFormatPDF,ObjMiss);
+
+            ObjDoc.Close();
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(ObjWord);
+            //ObjWord.Visible = true;
+        }
+
+        public void setTextToBookmark(Word.Document document, object bookmarkName, string text)
+        {
+            Word.Range bookmarkRange = document.Bookmarks.get_Item(ref bookmarkName).Range;
+            bookmarkRange.Text = text;
+            object rangeObj = bookmarkRange;
+            document.Bookmarks.Add((string)bookmarkName, ref rangeObj);
+       
+        }
+
+        public void replaceText(Word.Document doc, string oldText, string newText)
+        {
+            bool t = true;
+            bool f = false;
+            doc.Content.Find.Execute(oldText, f, t, f, f, f, t, 1, f, newText, 2,f, f, f, f);
+
+        }
+    }
+}
